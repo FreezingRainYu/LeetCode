b3e9e84c18a6a1c35bb498c72adffe4f1f16cf15
dy <liudongyu1119@outlook.com>
2019-04-11 15:00:29 +0800

    Update 0066PlusOne.java

--- Java/0066PlusOne.java
+++ Java/0066PlusOne.java
@@ -5,12 +5,12 @@ class Solution {
     public int[] plusOne(int[] digits) {
         digits[digits.length - 1]++;
         for (int i = digits.length - 1; i > 0; i--) {
-            if (digits[i] >= 10) {
+            if (digits[i] > 9) {
                 digits[i] -= 10;
                 digits[i - 1]++;
             }
         }
-        if (digits[0] >= 10) {
+        if (digits[0] > 9) {
             int[] answer = new int[digits.length + 1];
             System.arraycopy(digits, 1, answer, 1, digits.length - 1);
             answer[0] = 1;
